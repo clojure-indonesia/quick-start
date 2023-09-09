124b2423bc9516c9e4cc82c265f03de1cda7ec97
Ahmad Rasyid Salim <ahmadrasyidsalim@proton.me>
2023-09-10 00:37:39 +0700

    vcs: first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### COMPILE
+```bash
+clojure -M -m cljs.main -c quick-start.core
+```
+
+### RUN
+```bash
+python3 -m http.server
+```
+
+### REFERENCES
+- [https://clojurescript.org/guides/quick-start](https://clojurescript.org/guides/quick-start)
---
+++ src/quick_start/core.cljs
@@ -0,0 +1,24 @@
+(ns quick-start.core
+  (:require [goog.events :refer [listen]])
+  (:import [goog.events EventType]))
+
+(enable-console-print!)
+
+(println "quick-start")
+
+(defn fac [n]
+  (loop [cnt n
+         acc 1]
+    (if (zero? cnt)
+      acc
+      (recur (dec cnt)
+             (* acc cnt)))))
+
+(let [button (.querySelector js/document "button")
+      type EventType.CLICK]
+  (listen button type (fn [] (let [number (->> (.querySelector js/document "input[type=number]")
+                                               (.-value)
+                                               (js/parseInt))
+                                   msg (str "factorial " number " = " (fac number))
+                                   p (.querySelector js/document "p")]
+                               (set! (.-innerHTML p) msg)))))
